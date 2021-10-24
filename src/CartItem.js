487239be7b37eb465498646af0de36194976a64f
AshishBansal2101 <bansalashish2101@gmail.com>
2021-10-24 17:32:08 +0530

    Add Total Price

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-class CartItem extends React.Component{
-    render(){
-        console.log('this.props',this.props);
-        const {price,title,qty}=this.props.product;
+const CartItem = (props) => {
+        const {price,title,qty}=props.product;
         const {
             product,
             onIncreaseQuantity,
             onDecreaseQuantity,
             onDeleteProduct
-        }=this.props;
+        }=props;
         return(
           <div className="cart-item">
               <div className="left-block">
-               <img style={styles.image}/>
+               <img style={styles.image} src={product.img}/>
               </div>
               <div className="right-block">
                <div style={{fontSize:25}}>{title}</div>
@@ -40,8 +38,7 @@ class CartItem extends React.Component{
                </div>
               </div>
           </div>
-        );
-    }
+        )
 }
 
 const styles={
