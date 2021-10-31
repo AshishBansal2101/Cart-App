262d43b6d6e88ddc4b537029cd983dc40e6a7f1e
AshishBansal2101 <bansalashish2101@gmail.com>
2021-10-31 23:23:49 +0530

    add firebase and add product into firebase from app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import React from 'react'; 
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
+// import 'firebase';
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-        products:[
-            {
-                price: 99,
-                title: 'Watch',
-                qty: 1,
-                img:'https://images.unsplash.com/photo-1524592094714-0f0654e20314?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=799&q=80',
-                id:1
-            },
-            {
-                price: 999,
-                title: 'Mobile Phone',
-                qty: 10,
-                img:'https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80',
-                id:2
-            },
-            {
-                price: 999,
-                title: 'Laptop',
-                qty: 4,
-                img:'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=464&q=80',
-                id:3
-            }
-        ]
+        products:[],
+        loading:true
     }
+    this.db=firebase.firestore();
     // this.testing();
   //   this.increaseQuantity=this.increaseQuantity.bind(this);
+  }
+  componentDidMount(){
+    // firebase
+    // .firestore()
+    // .collection('products')
+    // .get()
+    // .then((snapshot)=>{
+    //   console.log(snapshot);
+    //   snapshot.docs.map((doc)=>{
+    //     console.log(doc.data())
+    //   });
+    //   const products=snapshot.docs.map((doc)=>{
+    //     const data=doc.data();
+ 
+    //     data['id'] = doc.id;
+    //     return data;
+    //   })
+    //   this.setState({
+    //     products,
+    //     loading:false
+    //   })
+    // })
+    this.db
+    .collection('products')
+    .onSnapshot((snapshot)=>{
+      console.log(snapshot);
+      snapshot.docs.map((doc)=>{
+        console.log(doc.data())
+      });
+      const products=snapshot.docs.map((doc)=>{
+        const data=doc.data();
+ 
+        data['id'] = doc.id;
+        return data;
+      })
+      this.setState({
+        products,
+        loading:false
+      })
+    })
   }
 handleIncreaseQuantity=(product)=>{
     console.log('Heyy please inc the qty of',product);
@@ -84,21 +106,42 @@ getCartTotal=()=>{
   let cartTotal=0;
 
   products.map((product)=>{
+    if(product.qty>0){
     cartTotal=cartTotal+product.qty*product.price
+    }
+    return '';
   })
   return cartTotal;
 }
+addProduct=()=>{
+  this.db
+  .collection('products')
+  .add({
+    img:'',
+    price:900,
+    qty:3,
+    title:'washing machine'
+  })
+  .then((docRef)=>{
+   console.log('Product has been added',docRef);
+  })
+  .catch((error)=>{
+    console.log('Error :', error);
+  })
+}
   render() {
-  const { products}=this.state;
+  const { products,loading}=this.state;
   return (
     <div className="App">
       <Navbar count={this.getCartCount()}/>
+      <button onClick={this.addProduct} style={{padding:20,fontSize:20}}>Add a product</button>
       <Cart
       products={products}
       onIncreaseQuantity={this.handleIncreaseQuantity}
       onDecreaseQuantity={this.handleDecreaseQuantity}
       onDeleteProduct={this.handleDeleteProduct}
       />
+      {loading && <h1>Loading Products....</h1>}
       <div style={{padding:10,fontSize:20}}>TOTAL:{this.getCartTotal()}</div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import * as firebase from 'firebase';
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCMf-Kun8hKXqQfy2fxWcInAs27fi8YWg",
+  authDomain: "cart-d635b.firebaseapp.com",
+  projectId: "cart-d635b",
+  storageBucket: "cart-d635b.appspot.com",
+  messagingSenderId: "357668036701",
+  appId: "1:357668036701:web:971ae9501e62a711ee6db4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <App />,
